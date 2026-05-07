cc58cd96f379b9f1ec555f240149469c26a3c808
EnterpriseGoose <olive@hackclub.com>
2026-05-07 15:14:47 -0700

    feat(guides): restyle guides to look like stardancy

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -1,4 +1,6 @@
 class GuidesController < ApplicationController
+  before_action :set_body_class
+
   def index
     @guides_by_category = Guide.by_category
     @category_order = Guide.category_order
@@ -7,5 +9,11 @@ class GuidesController < ApplicationController
   def show
     @guide = Guide.find_by_slug(params[:id])
     raise ActiveRecord::RecordNotFound if @guide.nil?
+  end
+
+  private
+
+  def set_body_class
+    @body_class = "app-layout-page"
   end
 end
