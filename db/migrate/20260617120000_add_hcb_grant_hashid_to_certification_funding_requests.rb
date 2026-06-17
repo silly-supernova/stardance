f6c8fc020293a67917bacfeddd771441d9e0d85c
Clay Nicholson <82461712+claynicholson@users.noreply.github.com>
2026-06-17 16:19:38 -0400

    Bruh

--- db/migrate/20260617120000_add_hcb_grant_hashid_to_certification_funding_requests.rb
+++ db/migrate/20260617120000_add_hcb_grant_hashid_to_certification_funding_requests.rb
@@ -1,4 +1,4 @@
-class AddHcbGrantHashidToCertificationFundingRequests < ActiveRecord::Migration[8.0]
+class AddHCBGrantHashidToCertificationFundingRequests < ActiveRecord::Migration[8.0]
   def change
     add_column :certification_funding_requests, :hcb_grant_hashid, :string
   end
